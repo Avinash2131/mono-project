297d5898253ca16898faf38fcd74f8ea63b02acb
root <root@ip-172-31-11-58.ap-south-1.compute.internal>
2023-09-19 09:54:17 +0000

    update ami and key

--- terraform/main.tf
+++ terraform/main.tf
@@ -6,6 +6,7 @@ secret_key = "F2ApVwfKONhnoggq3KwrNyMm3UWbp2KLdjov/KCM"
 resource "aws_instance" "key" {
 ami = "ami-0899663faf239dd8a"
 instance_type = "t2.micro"
+key_name = "projectkey"
 vpc_security_group_ids = [aws_security_group.demo-sg.id]
 tags = {
 Name = "project-instance"
