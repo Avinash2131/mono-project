fa385402cae951b13fa9ae783e5d56e451635736
Avinash2131 <129479942+Avinash2131@users.noreply.github.com>
2023-09-20 12:55:20 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,7 +1,7 @@
 provider "aws"  {
 region = "ap-south-1"
-access_key = "AKIAYINUTK2FBQ7C7LMV"
-secret_key = "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"
+access_key = "AKIAYINUTK2FM2EBJZEM"
+secret_key = "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"
 }
 resource "aws_instance" "key" {
 ami = "ami-0899663faf239dd8a"
